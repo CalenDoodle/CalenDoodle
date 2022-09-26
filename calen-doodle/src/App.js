fe3856984b56bc168ca72e85f52e6a843dde8839
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-09-26 13:47:54 -0700

    created a header and stored it in the home component

--- calen-doodle/src/App.js
+++ calen-doodle/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-  Home
+  <Home />
       </header>
     </div>
   );
---
+++ calen-doodle/src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from '../Header/Header';
+import './Home.css';
+
+function Home() {
+    return (
+        <div> 
+            <Header />
+            <h1> HIIIIIII</h1>
+        </div>
+    )
+}
+export default Home;
